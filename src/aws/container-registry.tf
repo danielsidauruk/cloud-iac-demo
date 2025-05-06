6b09fa50bf44b27822b175911fe722eff24787e8
Daniel H. Sidauruk <dev.danielsidauruk@gmail.com>
2025-05-07 00:36:08 +0700

    feat: add container

---
+++ src/aws/container-registry.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "main" {
+
+  for_each = toset(var.repository_list)
+
+  name                 = "ecr-${var.application_name}-${var.environment_name}-${each.value}"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  tags = {
+    application = var.application_name
+    environment = var.environment_name
+  }
+}
