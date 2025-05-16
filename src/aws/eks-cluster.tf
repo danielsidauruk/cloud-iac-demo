7f8fcfa2e07036f74a346d867997050901256d9f
Daniel H. Sidauruk <dev.danielsidauruk@gmail.com>
2025-05-16 22:56:47 +0700

    fix(eks): fix cluster subnet to private

---
+++ src/aws/eks-cluster.tf
@@ -0,0 +1,34 @@
+locals {
+  cluster_name       = "eks-${var.application_name}-${var.environment_name}"
+  cluster_subnet_ids = [for subnet in values(aws_subnet.private) : subnet.id]
+}
+
+resource "aws_eks_cluster" "main" {
+  name                      = local.cluster_name
+  role_arn                  = aws_iam_role.container_cluster.arn
+  enabled_cluster_log_types = ["api", "audit"]
+
+  vpc_config {
+
+    security_group_ids = [
+      aws_security_group.cluster.id,
+      aws_security_group.cluster_nodes.id
+    ]
+
+    subnet_ids              = local.cluster_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_controller_policy,
+    aws_cloudwatch_log_group.container_cluster,
+    aws_ecr_repository.main
+  ]
+
+  tags = {
+    application = var.application_name
+    environment = var.environment_name
+  }
+}
