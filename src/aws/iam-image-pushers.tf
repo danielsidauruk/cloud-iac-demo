a72593f098ccec14f17de562dc12cb36f2c30307
Daniel H. Sidauruk <dev.danielsidauruk@gmail.com>
2025-05-17 17:10:01 +0700

    feat(iam): add image-pusher role

---
+++ src/aws/iam-image-pushers.tf
@@ -0,0 +1,56 @@
+# Data source to create the policy document for assume role
+data "aws_iam_policy_document" "ecr_image_pusher_assume_role" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${var.ecr_image_pushers}"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Define the ECR image pusher policy document
+data "aws_iam_policy_document" "ecr_image_pusher" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload"
+    ]
+    resources = [for repo in aws_ecr_repository.main : repo.arn]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameter"]
+    resources = ["arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter/*"]
+  }
+}
+
+# IAM role for pushing images to ECR
+resource "aws_iam_role" "ecr_image_pusher" {
+  name               = "${var.application_name}-${var.environment_name}-ecr-image-pushers"
+  assume_role_policy = data.aws_iam_policy_document.ecr_image_pusher_assume_role.json
+}
+
+# IAM policy for ECR image pushing
+resource "aws_iam_policy" "ecr_image_pusher" {
+  name   = "${var.application_name}-${var.environment_name}-ecr-image-pusher"
+  policy = data.aws_iam_policy_document.ecr_image_pusher.json
+}
+
+# Attach the policy to the IAM role
+resource "aws_iam_role_policy_attachment" "ecr_image" {
+  policy_arn = aws_iam_policy.ecr_image_pusher.arn
+  role       = aws_iam_role.ecr_image_pusher.name
+}
