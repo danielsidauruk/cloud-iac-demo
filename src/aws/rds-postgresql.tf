f95380f7613b7c2c1da0b636884837679caa3a92
Daniel H. Sidauruk <dev.danielsidauruk@gmail.com>
2025-05-17 19:22:10 +0700

    feat(rds): add rds instance & sg

---
+++ src/aws/rds-postgresql.tf
@@ -0,0 +1,23 @@
+resource "aws_db_subnet_group" "rds" {
+  name       = "${var.application_name}-${var.environment_name}-rds-subnet-group"
+  subnet_ids = [for subnet in aws_subnet.private : subnet.id]
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier             = "${var.application_name}-${var.environment_name}-postgres"
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = var.postgres_username
+  password               = random_password.database_connection_string.result
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  apply_immediately      = true
+
+  tags = {
+    application = var.application_name
+    environment = var.environment_name
+  }
+}
---
+++ src/aws/security-group-rds.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "rds" {
+  name        = "${var.application_name}-${var.environment_name}-rds-sg"
+  description = "Allow PostgreSQL access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
