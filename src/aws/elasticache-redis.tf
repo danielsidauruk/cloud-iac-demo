3d7b9958fc7121839da63ee7258e7738fd22522b
Daniel H. Sidauruk <dev.danielsidauruk@gmail.com>
2025-05-17 20:15:20 +0700

    feat(elasticache): add Redis OSS caches

---
+++ src/aws/elasticache-redis.tf
@@ -0,0 +1,16 @@
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "${var.application_name}-${var.environment_name}-redis-subnet-group"
+  subnet_ids = [for subnet in aws_subnet.private : subnet.id]
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "${var.application_name}-${var.environment_name}-redis"
+  engine               = "redis"
+  engine_version       = "6.x"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x" # or your custom Redis 6.x group
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name
+  security_group_ids   = [aws_security_group.redis.id]
+}
---
+++ src/aws/security-group-redis.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "redis" {
+  name        = "${var.application_name}-${var.environment_name}-redis-sg"
+  description = "Allow Redis access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
