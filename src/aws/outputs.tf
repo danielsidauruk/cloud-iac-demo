2cc47f63436fc338db5b187855c719a38c5f1900
danielsidauruk <danielsidauruk@users.noreply.github.com>
2025-06-30 15:04:17 +0700

    👷 feat: add Terraform Outputs

--- src/aws/outputs.tf
+++ src/aws/outputs.tf
@@ -11,6 +11,71 @@ output "environment_name" {
 
 output "primary_region" {
   value       = var.primary_region
+   description = "Primary AWS region."
+}
+
+
+# Kubernetes Configuration
+output "kubernetes_cluster_name" {
+  value       = module.eks.kubernetes_cluster_name
+  description = "EKS cluster name."
+}
+
+output "kubernetes_namespace" {
+  value       = var.kubernetes_namespace
+  description = "Kubernetes namespace."
+}
+
+output "kubernetes_service_account_name" {
+  value       = var.kubernetes_service_account_name
+  description = "Kubernetes service account name."
+}
+
+# IAM Roles
+output "alb_controller_role" {
+  value       = module.eks.alb_controller_role_arn
+  description = "ALB Controller IAM role ARN."
+}
+
+output "workload_identity_role" {
+  value       = module.eks.workload_identity_role
+  description = "ARN of the K8s Workload Identity (IRSA) role."
+}
+
+# Service Credentials & Endpoints
+output "bucket_name" {
+  value       = var.bucket_name
+  description = "S3 bucket name."
+}
+
+output "postgres_host_endpoint" {
+  value       = module.rds.postgres_host_endpoint
+  description = "PostgreSQL database endpoint."
+}
+
+output "rabbitmq_host_endpoint" {
+  value       = module.mq.rabbitmq_host_endpoint
+  description = "RabbitMQ broker endpoint."
+}
+
+output "redis_host_endpoint" {
+  value       = module.elasticache.redis_host_endpoint
+  description = "Redis ElastiCache endpoint."
+}
+
+# General Application & Environment
+output "application_name" {
+  value       = var.application_name
+  description = "Application's name."
+}
+
+output "environment_name" {
+  value       = var.environment_name
+  description = "Deployment environment."
+}
+
+output "primary_region" {
+  value       = var.primary_region
   description = "The primary AWS region."
 }
 
@@ -38,10 +103,20 @@ output "alb_controller_role" {
 
 output "workload_identity_role" {
   value       = module.eks.workload_identity_role
-  description = "ARN of the K8s Workload Identity (IRSA) role."
+  description = "Workload Identity (IRSA) role ARN."
 }
 
 # Service Credentials & Endpoints
+output "username" {
+  value       = var.username
+  description = "Default admin username."
+}
+
+output "postgres_dbname" {
+  value       = var.postgres_dbname
+  description = "PostgreSQL database name."
+}
+
 output "bucket_name" {
   value       = var.bucket_name
   description = "S3 bucket name."
